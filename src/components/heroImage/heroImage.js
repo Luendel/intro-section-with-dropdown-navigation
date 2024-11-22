0e3ade8eb408a66876f2a15dc6b5c61d6250c9fa
Luendel <luendel3@hotmail.com>
2024-11-22 11:19:56 -0300

    modified:   src/components/heroImage/heroImage.js modified:   src/components/heroText/heroText.css modified:   src/components/heroText/heroText.js modified:   src/components/navbar/navbar.js

--- src/components/heroImage/heroImage.js
+++ src/components/heroImage/heroImage.js
@@ -3,7 +3,8 @@ import React from "react";
 function HeroImage() {
  return(
     <div className="container-fluid w-100 m-0 p-0">
-        <img src="./images/image-hero-mobile.png" className="img-fluid mt-2" style={{width:"100vw"}} alt="man working on a laptop"/>
+        <img src="./images/image-hero-mobile.png" className="img-fluid mt-2 d-sm-block d-lg-none" style={{width:"100vw"}} alt="man working on a laptop"/>
+        <img src="./images/image-hero-desktop.png" className="mt-2 d-none d-lg-block" style={{width:"75%", height:"90%", margin:"0 auto"}} alt="man working on a laptop"/>
     </div>
  )   
 }
